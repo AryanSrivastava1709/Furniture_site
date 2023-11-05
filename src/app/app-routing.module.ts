d3c842f6b38312dd517150921eab14ee751d9c75
Aryan Srivastava <Aryansrivas123@gmail.com>
2023-11-05 12:31:49 +0530

    Completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,12 @@ import { LoginComponent } from './components/login/login.component';
 import { WebsiteHeaderComponent } from './components/website-header/website-header.component';
 import { ProductPageComponent } from './components/product-page/product-page.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { SerperateProdPageComponent } from './components/product-page/serperate-prod-page/serperate-prod-page.component';
 const routes: Routes = [
   { path: 'cart', component: LoginComponent },
   { path: 'main', component: WebsiteHeaderComponent },
   { path: 'product', component: ProductPageComponent },
   { path: 'about', component: AboutUsComponent },
-  { path: 'contact', component: ContactUsComponent },
   { path: '', component: WebsiteHeaderComponent },
   {
     path: 'product/individualproduct/:id',
